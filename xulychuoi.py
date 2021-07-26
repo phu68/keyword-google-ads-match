e3da525d75b29f2a0e5f257777b5c7e6ec76a469
KS Phu <thanhphupham@gmail.com>
2021-07-26 22:26:00 +0700

    chia ham xu ly chuoi

--- xulychuoi.py
+++ xulychuoi.py
@@ -3,23 +3,47 @@
 
 from typing import Counter
 
+def tach_chuoi(chuoi):
+    chuoi_moi = list(chuoi.split(" "))
+    return chuoi_moi
 
-def split_line(text):
-
-    # split the text
-    words = list(text.split(" "))
-
+def doi_sanh_rong_co_dk(chuoi):
+    chuoi_moi = list(chuoi.split(" "))
     i = 0
-    for word in text.split():
-        words[i] = "+"+words[i]
-        i+=1
+    for tu in chuoi.split():
+        chuoi_moi[i] = "+"+chuoi_moi[i]
+        i += 1
+    str1 = ' '.join(chuoi_moi)
+    # print(str1)
+    return str1
+
+
+def chuyen_str(chuoi):
+    str1 = ' '.join(chuoi)
+    return str1
+
+
+# def split_line(text):
+
+#     # split the text
+#     words = list(text.split(" "))
+
+#     i = 0
+#     for word in text.split():
+#         words[i] = "+"+words[i]
+#         i+=1
     
-    str1 = ' '.join(words)
-    print(str1)
+#     str1 = ' '.join(words)
+#     print(str1)
     # for each word in the line:
     # for word in words:
     #     # print the word
     #     words.append(word)
     #     print("+",words)
 if __name__ == "__main__":
-    split_line("phạm thanh phú")
+    #split_line("phạm thanh phú")
+    phu = "phạm thanh phú"
+    # tach_chuoi(phu)
+    kq = doi_sanh_rong_co_dk(phu)
+    # chuyen_str(phu)
+    print(kq)
